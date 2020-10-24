474efd4c5a57bb7d1bc41c8819b400bdf8649c5a
mehranazam <mehranazam03@gmail.com>
2020-10-24 21:32:44 +0500

    update

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import "../App.css"
-class Banner extends React.Component{
-    render(){
+export default function Banner(){
                 return <img src={require("../img/banner.PNG")} className="Banner" alt="image of banner" />
-    }
-}
 
-export default Banner;
+}
