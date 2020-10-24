9c2a9b9029e61e8b5eacea03f9b48b90a339b13d
mehranazam <mehranazam03@gmail.com>
2020-10-24 21:35:34 +0500

    update

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../App.css"
 export default function Banner(){
-                return <img src={require("../img/banner.PNG")} className="Banner" alt="image of banner" />
+                return <img src={require("../img/banner.PNG")} className="Banner" alt="banner" />
 
 }
