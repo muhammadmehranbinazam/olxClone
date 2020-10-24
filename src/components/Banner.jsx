65e5151e6bf3bc7c30078499984f6bd3b319d460
mehranazam <mehranazam03@gmail.com>
2020-10-24 21:26:16 +0500

    Error update

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,11 +3,7 @@ import BannerImage from "../img/banner.PNG"
 import "../App.css"
 class Banner extends React.Component{
     render(){
-        return(
-            <div>
-                <img src={BannerImage} className="Banner" alt="image of banner"/>
-            </div>
-        );
+                return <img src={BannerImage} className="Banner" alt="image of banner" />
     }
 }
 
